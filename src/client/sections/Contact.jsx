9a0bf2b12f60d1b9d4247782bbf9040a98fb68b2
Mskauen <mathias.skauen.harestad@gmail.com>
2022-04-08 05:04:47 +0200

    Update Contact.jsx

--- src/client/sections/Contact.jsx
+++ src/client/sections/Contact.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function Contact () {
-    return <div className="space"/>
+    return (
+        <div className="contact">
+            <div className="contact-innerRef"/>
+            <h1>Contact Me</h1>
+            <form action="mailto:info@w3docs.com" method="get" encType="text/plain">
+                <div>
+                    <input type="text" name="name" id="name" placeholder="Name"/>
+                </div>
+                <div>
+                    <input type="text" name="email" id="email" placeholder="Enter email"/>
+                </div>
+                <div>
+                    <textarea name="comments" rows="12" cols="35">Your message.</textarea>
+                </div>
+                <div>
+                    <input type="submit" name="submit" value="Get In Touch"/>
+                </div>
+            </form>
+        </div>
+    )
 }
