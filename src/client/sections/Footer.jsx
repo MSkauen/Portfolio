87dbb56b68d33054d8a4d90d99cfadf6bae95b80
Mskauen <mathias.skauen.harestad@gmail.com>
2022-04-11 05:23:59 +0200

    Update Footer.jsx

--- src/client/sections/Footer.jsx
+++ src/client/sections/Footer.jsx
@@ -3,7 +3,7 @@ import "../../shared/css/Footer.css";
 
 export default function Footer () {
     return (
-        <div className="section footer">
+        <div className="footer">
             <a href={"https://github.com/MSkauen/Portfolio"}>Designed & Developed by Mathias Skauen Harestad</a>
         </div>
 
