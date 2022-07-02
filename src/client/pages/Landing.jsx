b685a98fa919b2ad3ca2848eaf4df6d80416a222
Mskauen <mathias.skauen.harestad@gmail.com>
2022-07-02 02:23:52 +0200

    Minor fix

--- src/client/pages/Landing.jsx
+++ src/client/pages/Landing.jsx
@@ -3,14 +3,13 @@ import "../../shared/css/App.css";
 import "../../shared/css/FadeInSection.css";
 import background from "url:../../shared/img/background.png";
 import {ParticlesContainer as Links} from "../components/Links";
-import Background from "../lib/background.jsx";
+import Background from "../lib/Background.jsx";
 import NavBar from "../components/Navbar";
 import Hero from "../sections/Hero";
 import Projects from "../sections/Projects";
 import Skills from "../sections/Skills";
 import Contact from "../sections/Contact";
 import Footer from "../sections/Footer";
-import Space from "../sections/Space";
 
 export default function Landing () {
     return (
@@ -21,7 +20,6 @@ export default function Landing () {
                 <Hero/>
                 <Projects/>
                 <Skills/>
-                <Space/>
                 <Contact/>
             <Footer/>
         </div>
